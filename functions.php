ce858712dc98634748b53acde5f848d9575d0c94
Florian Brinkmann <kontakt@florianbrinkmann.com>
2022-05-06 13:59:16 +0200

    ✨ added a few last modifications

--- functions.php
+++ functions.php
@@ -15,3 +15,13 @@ if ( ! function_exists( 'wh_block_enqueue_assets' ) ) {
 	}
 }
 add_action( 'wp_enqueue_scripts', 'wh_block_enqueue_assets' );
+
+if ( ! function_exists( 'wh_block_load_textdomain' ) ) {
+	/**
+	 * Load text domain.
+	 */
+	function wh_block_load_textdomain() {
+		load_theme_textdomain( 'wh-block', get_template_directory() . '/languages' );
+	}
+}
+add_action( 'after_setup_theme', 'wh_block_load_textdomain' );
